016f3fef26580ced08d54a74ca12a2e8c835069a
ealipkin <evgeny2@rambler.ru>
2020-09-22 22:22:34 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,351 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const log = require('fancy-log');
+const minimist = require('minimist');
+const pug = require('gulp-pug');
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const prettyHtml = require('gulp-pretty-html');
+const imagemin = require('gulp-imagemin');
+const svgstore = require('gulp-svgstore');
+const svgmin = require('gulp-svgmin');
+const changed = require('gulp-changed');
+const rename = require("gulp-rename");
+const sass = require('gulp-sass');
+const inject = require('gulp-inject');
+const babel = require('gulp-babel');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const bust = require('gulp-buster');
+const argv = minimist(process.argv.slice(2));
+const blocksFolder = 'src/blocks';
+const tplFolder = 'templates';
+const path = require('path');
+const fs = require('fs');
+const projectName = 'Ai Mori';
+
+const DEFAULT_PAGE_NAME = 'index';
+
+let pageName = DEFAULT_PAGE_NAME;
+
+// images
+gulp.task('images-optimize',
+  () => gulp.src('src/images/**/*.+(png|jpg|gif|svg)').pipe(imagemin()).pipe(gulp.dest('src/images'))
+);
+gulp.task('images-copy', () => gulp.src('src/images/**/*.+(png|jpg|gif|svg)').pipe(gulp.dest('dest/images')));
+
+gulp.task('images', gulp.series('images-optimize', 'images-copy'));
+
+
+// SVG
+const buildSvg = () => {
+  const target = gulp.src('src/pug/svg.pug');
+  const source = gulp
+    .src('src/images/svg/*.svg')
+    .pipe(svgmin())
+    .pipe(svgstore({inlineSvg: true}));
+
+  return target
+    .pipe(inject(source, {transform: fileContents}))
+    .pipe(gulp.dest('src/pug/'));
+};
+gulp.task('svg', buildSvg);
+
+gulp.task('svgInclude', () =>
+  gulp
+    .src('src/images/svg/*.svg')
+    .pipe(svgmin())
+    .pipe(svgstore())
+    .pipe(gulp.dest('dest/svg/')));
+
+gulp.task('svgmin', () => {
+  gulp
+    .src('src/images/svg/*.svg')
+    .pipe(svgmin({
+      plugins: [{
+        removeTitle: true
+      }]
+    }))
+    .pipe(gulp.dest('src/images/svg/'));
+
+  gulp
+    .src('src/images/icons/*.svg')
+    .pipe(svgmin({
+      plugins: [{
+        removeTitle: true
+      }]
+    }))
+    .pipe(gulp.dest('src/images/icons/'));
+});
+
+//fonts
+gulp.task('fonts', () => gulp.src('src/fonts/*').pipe(gulp.dest('dest/fonts')));
+
+//CSS
+gulp.task('sass', () =>
+  gulp
+    .src([
+      'src/css/variables.scss',
+      'src/css/vendor/*.scss',
+      'src/css/base.scss',
+      'src/blocks/base/**/*.scss',
+      'src/blocks/composite/**/*.scss',
+      'src/css/utils.scss'
+    ])
+    .pipe(concat('main.css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css/compiled'))
+);
+
+const buildCss = () =>
+  gulp.src('src/css/compiled/main.css')
+      .pipe(rename('main.css'))
+      .pipe(sourcemaps.init())
+      .pipe(postcss([require('autoprefixer')]))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('dest/css/'))
+      .pipe(browserSync.stream({match: "*.css"}));
+
+gulp.task('css', gulp.series('sass', buildCss));
+
+gulp.task('css-minify', () =>
+  gulp.src('dest/css/*.css')
+      .pipe(sourcemaps.init())
+      .pipe(cleanCSS())
+      .pipe(sourcemaps.write('/'))
+      .pipe(gulp.dest('dest/css/'))
+);
+// CSS END
+
+
+//JS
+const buildVendor = () =>
+  gulp
+    .src([
+      // 'src/js/jquery.min.js',
+      'src/js/vendor/*.js',
+      // 'src/js/core.js'
+    ])
+    .pipe(concat('_vendor-code.js')).pipe(gulp.dest('src/js/_compiled/'));
+
+gulp.task('js-build:vendor', buildVendor);
+
+const buildProject = () =>
+  gulp
+    .src([
+      'src/js/initial.js',
+      'src/blocks/base/**/*.js',
+      'src/blocks/composite/**/*.js'
+    ])
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(concat('_project-code.js'))
+    .pipe(gulp.dest('src/js/_compiled/'));
+
+gulp.task('js-build:project', buildProject);
+
+const buildJs = () => {
+  return gulp
+    .src([
+      'src/js/_compiled/_vendor-code.js',
+      'src/js/_compiled/_project-code.js'
+    ])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dest/js/'))
+    .pipe(gulp.dest('src/'));
+};
+
+gulp.task('js', gulp.series('js-build:vendor', 'js-build:project', buildJs));
+
+const uglifyJs = () =>
+  gulp
+    .src('dest/js/main.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('/'))
+    .pipe(gulp.dest('dest/js/'));
+
+gulp.task('js-uglify', uglifyJs);
+
+//JS END
+
+// UTILS
+gulp.task('buster', () =>
+  gulp
+    .src([
+      'dest/js/main.js',
+      'dest/css/main.css'
+    ])
+    .pipe(bust())
+    .pipe(gulp.dest('.')));
+// UTILS END
+
+
+// PUG
+gulp.task('prettifyHtml', () =>
+  gulp.src('dest/*.html')
+      .pipe(prettyHtml())
+      .pipe(gulp.dest('dest'))
+);
+
+gulp.task('pug-concat', () =>
+  gulp.src(['src/blocks/*/*/*.pug'])
+      .pipe(plumber())
+      .pipe(concat('_mixins.pug'))
+      .pipe(gulp.dest('src/pug/')));
+
+const buildPug = () =>
+  gulp.src('src/pug/views/*.pug')
+      .pipe(changed('dest', {extension: '.html'}))
+      .pipe(plumber())
+      .pipe(pug())
+      .pipe(gulp.dest('dest'));
+
+gulp.task('pug', gulp.series('svg', 'pug-concat', buildPug));
+
+const buildPugPage = (done) => {
+  gulp.src(`src/pug/views/${pageName}.pug`)
+      .pipe(plumber())
+      .pipe(pug())
+      .pipe(gulp.dest('dest'));
+  done();
+};
+
+gulp.task('pug-page', gulp.series('pug-concat', buildPugPage));
+
+// PUG END
+
+const completeProd = (done) => {
+  log('static build success in production mode');
+  done();
+};
+//Таск работает долго из-за оптимизации изображений, для быстрой сборки использовать static
+gulp.task('prod',
+  gulp.series(
+    'images-copy',
+    'fonts',
+    'css',
+    'css-minify',
+    'js',
+    'js-uglify',
+    'buster',
+    'pug',
+    'prettifyHtml',
+    'svgInclude',
+    completeProd)
+);
+gulp.task('static',
+  gulp.series('images-copy', 'fonts', 'css', 'js', 'pug', 'pug-concat', (done) => {
+    log('static build success in develop mode');
+    done();
+  })
+);
+
+gulp.task('browser-sync', (done) => {
+  browserSync.init({
+    server: {
+      baseDir: './dest',
+      startPath: `/${pageName}.html`,
+    },
+    notify: false
+  });
+  // browserSync.watch('dest/').on('change', browserSync.reload);
+  done();
+});
+
+//watchers and static server
+gulp.task('watch', gulp.series('browser-sync', (done) => {
+    gulp.watch(['src/**/*.scss'], gulp.series('css'));
+
+    gulp.watch(['src/js/*.js', 'src/js/vendor/*.js', 'src/blocks/**/**/*.js'], gulp.series('js'));
+
+    gulp.watch(['src/blocks/*/*/*.pug', 'src/pug/layouts/*.pug', 'src/pug/views/*.pug'], gulp.series('pug-page'));
+
+    gulp.watch(`dest/${pageName}.html`).on('change', browserSync.reload);
+    gulp.watch(`dest/js/main.js`).on('change', browserSync.reload);
+    gulp.watch('dest/css/main.css').on('change', () => browserSync.reload('*.css'));
+    done();
+  }
+))
+;
+
+const setPageName = (done) => {
+  pageName = argv.name ? argv.name.split(',')[0] : DEFAULT_PAGE_NAME;
+  done();
+};
+
+//reload one page
+gulp.task('page', gulp.series(setPageName, 'pug', 'js', 'css', 'watch'));
+
+
+//create block (use - gulp create --name=%NAME% --type=b --tech=%tech%)
+gulp.task('create', (done) => {
+  const name = argv.name && argv.name.split(',')[0];
+  const tech = argv.tech ? argv.tech.split(',') : ['pug', 'scss', 'js'];
+  const blockTypeFolder = argv.type === 'b' ? 'base' : 'composite';
+
+  // создаваемый блок обязан иметь имя
+  if (!name.length) {
+    return log('Введите имя блока');
+  }
+
+  const blockFolder = path.normalize(path.join(blocksFolder, blockTypeFolder, name)); // папка для конкретного блока
+  return fs.access(blockFolder, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+      if (!err) {
+        return log(`Блок с именем «${name}» уже существует`);
+      } else {
+        if (!fs.existsSync(blockFolder)) {
+          fs.mkdirSync(blockFolder);
+        }
+        createTechFile(name, blockFolder, tech);
+      }
+      done();
+    }
+  );
+});
+const getTemplate = (tech) => fs.readFileSync(path.join(tplFolder, tech + '.txt'), 'utf8');
+
+function createTechFile(blockName, folderPath, tech) {
+  let tpl;
+  const jsBlockName = blockName
+    .split('-')
+    .reduce((previousValue, currentItem, index) => {
+      if (index > 0) {
+        currentItem = currentItem[0].toUpperCase() + currentItem.slice(1)
+      }
+      return previousValue + currentItem
+    });
+
+  for (let j = 0; j < tech.length; j++) {
+    const filePath = folderPath + "/" + blockName + "." + tech[j];
+    switch (tech[j]) {
+      case 'pug':
+        tpl = getTemplate('pug').replace(/{{blockname}}/g, blockName);
+        break;
+      case 'scss':
+        tpl = getTemplate('css').replace(/{{blockname}}/g, blockName);
+        break;
+      //temporary disable generate js files
+      case 'js':
+        tpl = getTemplate('js').replace(/{{blockname}}/g, jsBlockName).replace(/{{projectname}}/g, projectName);
+        break;
+      default:
+        tpl = '';
+    }
+
+    fs.writeFile(filePath, tpl, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`Блок ${blockName} успешно создан (${tech[j]})`);
+    })
+    ;
+  }
+}
+
+function fileContents(filePath, file) {
+  return file.contents.toString();
+}
---
+++ src/js/initial.js
@@ -0,0 +1,204 @@
+const PREVIEW_URL = 'https://img.youtube.com/vi';
+const PreviewUrls = {
+  DEFAULT: 'default.jpg', //
+  HIGH: 'hqdefault.jpg', // For the high quality version of the thumbnail use a URL similar to this
+  MEDIUM: 'mqdefault.jpg', //  There is also a medium quality version of the thumbnail, using a URL similar to the HQ
+  STANDART: 'sddefault.jpg', // For the standard definition version of the thumbnail, use a URL similar to this
+  MAX: 'maxresdefault.jpg', // For the maximum resolution version of the thumbnail use a URL similar to this
+};
+
+const SLIDE_IMAGE = PreviewUrls.MEDIUM;
+const BACKGROUND_IMAGE = PreviewUrls.STANDART;
+const ACTIVE_BUTTON_CLASS = 'videos__video-button--active';
+
+const videos = [
+  {
+    title: "No Doubt - Don't Speak RUS COVER/НА РУССКОМ",
+    href: "https://www.youtube.com/watch?v=qRLaYz4Azf8"
+  },
+  {
+    title: "Motionless In White - Voices RUS COVER/КАВЕР НА РУССКОМ (feat. Ник Черников)",
+    href: "https://www.youtube.com/watch?v=iTLK1gMf-LE"
+  },
+  {
+    title: "LINKIN PARK - One More Light RUS COVER/КАВЕР НА РУССКОМ",
+    href: "https://www.youtube.com/watch?v=-v_MEQ6naJA"
+  },
+  {
+    title: "Skillet - Hero RUS COVER/КАВЕР НА РУССКОМ (feat. BugaGamesTV)",
+    href: "https://www.youtube.com/watch?v=a-1y-p52gwQ"
+  },
+  {title: "Rag'n'Bone Man - Human RUS COVER/ НА РУССКОМ КАВЕР", href: "https://www.youtube.com/watch?v=pYp9y2NZURc"},
+  {title: "LINKIN PARK - GIVEN UP RUS/ НА РУССКОМ КАВЕР", href: "https://www.youtube.com/watch?v=m2leRAXFbAc"},
+  {
+    title: "Three Days Grace - Over and Over RUS COVER/ КАВЕР НА РУССКОМ ЯЗЫКЕ",
+    href: "https://www.youtube.com/watch?v=-efKUN4XIoM"
+  },
+  {
+    title: "BRING ME THE HORIZON  -  SHADOW MOSES RUS COVER/НА РУССКОМ В ЖЕНСКОЙ ВЕРСИИ",
+    href: "https://www.youtube.com/watch?v=r-X6ZNPqhbc"
+  },
+  // {title: "КАРАНТИНО (Веселая песня про карантин by Ai Mori) #карантиновидение2020", href: "https://www.youtube.com/watch?v=GOsa15ZPeNI"},
+  {title: "The Killers - Mr. Brightside RUS/НА РУССКОМ", href: "https://www.youtube.com/watch?v=BbjdNQQpT7o"},
+  {title: "Drowning Pool - Tear Away RUS/ РУССКИЙ КАВЕР", href: "https://www.youtube.com/watch?v=PosxJAVMCMo"},
+  {title: "30  Seconds To Mars - The Kill RUS cover НА РУССКОМ", href: "https://www.youtube.com/watch?v=hz0bmNkyyUs"},
+  {title: "The Rasmus - In the Shadows RUS/РУССКИЙ КАВЕР", href: "https://www.youtube.com/watch?v=KUz7SQaaxWc"},
+  {title: "Tokio Hotel - Monsoon RUS/РУССКИЙ КАВЕР", href: "https://www.youtube.com/watch?v=lJlaZmL37kY"},
+  {title: "The Pretty Reckless - Just Tonight RUS/РУССКИЙ КАВЕР", href: "https://www.youtube.com/watch?v=YbKMQJ1-xwE"},
+  {
+    title: "Open Door - Mike Shinoda feat. Ai Mori #SingOpenDoor",
+    href: "https://www.youtube.com/watch?v=M6IachIhOb8"
+  },
+  {
+    title: "Breaking The Habit - Linkin Park  НА РУССКОМ/ RUS COVER",
+    href: "https://www.youtube.com/watch?v=9vdpNoarFJU"
+  },
+  {
+    title: "My Heart Will Go On (Céline Dion RUS COVER / РУССКАЯ ВЕРСИЯ OST ТИТАНИК)",
+    href: "https://www.youtube.com/watch?v=pEN2dCahQXg"
+  },
+  {
+    title: "Architects - Royal Beggars RUS COVER / РУССКАЯ ВЕРСИЯ",
+    href: "https://www.youtube.com/watch?v=FRupDpVDJiQ"
+  },
+  {
+    title: "Green Day - Boulevard Of Broken Dreams (RUS COVER/ НА РУССКОМ КАВЕР)",
+    href: "https://www.youtube.com/watch?v=Mc62sKuhSLs"
+  },
+  {
+    title: "Roxette - Listen To Your Heart (ROCK RUS COVER / НА РУССКОМ РОК )",
+    href: "https://www.youtube.com/watch?v=NOCYGQaWCMM"
+  },
+  {title: "LINKIN PARK - CRAWLING RUS COVER / РУССКАЯ ВЕРСИЯ", href: "https://www.youtube.com/watch?v=rGy1UgqmBzQ"},
+  {
+    title: "BRITNEY SPEARS - TOXIC  РОК ВЕРСИЯ НА РУССКОМ/ ROCK RUSSIAN VERSION",
+    href: "https://www.youtube.com/watch?v=A26wP9LqG84"
+  },
+  {title: "Flyleaf - I'm So Sick  RUS COVER / РУССКАЯ ВЕРСИЯ", href: "https://www.youtube.com/watch?v=5KCMOVjez64"},
+  {
+    title: "BRING ME THE HORIZON -  SLEEPWALKING | RUS COVER / НА РУССКОМ ЯЗЫКЕ В ЖЕНСКОЙ ВЕРСИИ",
+    href: "https://www.youtube.com/watch?v=EmOdHWz-vtw"
+  },
+  {
+    title: "ВЕДЬМАКУ ЗАПЛАТИТЕ ЧЕКАННОЙ МОНЕТОЙ укулеле ( Ft. Глад Валакас, Геральт из Ривии)",
+    href: "https://www.youtube.com/watch?v=vPVQZJwPLLo"
+  },
+  {title: "В ЛЕСУ РОДИЛАСЬ ЕЛОЧКА| Rock версия", href: "https://www.youtube.com/watch?v=MfOvc50foQM"},
+  {title: "MAD WORLD НА РУССКОМ (RUSSIAN VERSION)", href: "https://www.youtube.com/watch?v=DSwoYP90xRE"},
+  {title: "EVANESCENCE НА РУССКОМ (RUSSIAN VERSION)", href: "https://www.youtube.com/watch?v=4_Pj9idMxGI"},
+  {title: "LINKIN PARK НА РУССКОМ ЯЗЫКЕ В ЖЕНСКОЙ ВЕРСИИ", href: "https://www.youtube.com/watch?v=1PCKOis6EQY"},
+  {title: "KATY PERRY НА РУССКОМ ЯЗЫКЕ (ft. Женя Hawk)", href: "https://www.youtube.com/watch?v=jxcFI3Qnkus"},
+  {title: "AVRIL LAVIGNE НА РУССКОМ ЯЗЫКЕ", href: "https://www.youtube.com/watch?v=UXlpXwP0LoU"},
+  {title: "Ai Mori ft. HEYLEE, ДНК, INS1DE - BAN TWITCH", href: "https://www.youtube.com/watch?v=K7mXZXePMZ4"},
+  {title: "Billie Eilish НА РУССКОМ ЯЗЫКЕ", href: "https://www.youtube.com/watch?v=FxaA4R4YHmo"},
+
+  // Блогеры
+  {title: "itpedia : 50 ОТТЕНКОВ ШЕВЦОВА", href: "https://www.youtube.com/watch?v=b__rJ_yAcSk"},
+  {title: "Ai Mori и Глад Валакас (Feat. Midix)", href: "https://www.youtube.com/watch?v=PIh7aMRBaJI"},
+  {title: "Ai Mori и olyashaa", href: "https://www.youtube.com/watch?v=ZwJ9G-nhRjU"},
+];
+
+const videoPlayer = document.querySelector('.video-container');
+const playOverlay = document.querySelector('.videos__play-overlay');
+const videoPreloader = document.querySelector('.videos__preloader');
+const videoBackground = document.querySelector('.videos__background');
+
+function parseVideoId(video) {
+  return video.href.split('v=').pop();
+}
+
+function generateSlides(videos) {
+  return videos.reduce((acc, video) => {
+    const videoId = parseVideoId(video);
+    return acc +
+      `<div class="swiper-slide">
+        <div class="swiper-lazy-preloader"></div>
+        <button type="button" data-id="${videoId}" class="videos__video-button">
+          <img data-src="${PREVIEW_URL}/${videoId}/${SLIDE_IMAGE}" alt="${video.title}" class="swiper-lazy">
+        </button>
+      </div>`;
+  }, '');
+}
+
+function fillSlider(videos) {
+  const slider = document.querySelector('.swiper-wrapper');
+  const markup = generateSlides(videos);
+  slider.insertAdjacentHTML('afterbegin', markup);
+}
+
+function runVideo(id) {
+  const iframe = `<iframe src="https://www.youtube.com/embed/${id}?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="allowfullscreen"></iframe>`;
+  videoBackground.style.backgroundImage = `url(${PREVIEW_URL}/${id}/${BACKGROUND_IMAGE})`;
+  videoPlayer.innerHTML = '';
+  videoPlayer.insertAdjacentHTML('afterbegin', iframe);
+}
+
+function hideOverlay() {
+  playOverlay.classList.add('videos__play-overlay--hidden');
+  videoPreloader.classList.add('videos__preloader--visible');
+}
+
+(function () {
+  const player = new Playerjs({
+    id: 'player',
+    file: [
+      {title: 'Пластилин', file: '/audio/AiMori-Plastilin.mp3', poster: 'images/covers/plastilin_thumb.jpg'},
+      {title: 'Капелька летней грусти', file: '/audio/AiMori-Kapelka.mp3', poster: 'images/covers/kapelka_thumb.jpg'},
+      {title: 'Электро', file: 'audio/AiMori-Elektro.mp3', poster: 'images/covers/electro_thumb.jpg'},
+      {title: 'Тройничек', file: '/audio/AiMori-Troinichek.mp3', poster: 'images/covers/troinichek_thumb.jpg'},
+    ]
+  });
+  fillSlider(videos);
+  const videosButtons = document.querySelectorAll('.videos__video-button');
+  videosButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      const activeButton = document.querySelector(`.${ACTIVE_BUTTON_CLASS}`);
+      const target = event.currentTarget;
+      const videoId = target.dataset.id;
+      hideOverlay();
+      if (activeButton) {
+        activeButton.classList.remove(ACTIVE_BUTTON_CLASS);
+      }
+      target.classList.add(ACTIVE_BUTTON_CLASS);
+      runVideo(videoId);
+    });
+  });
+
+  const DEFAULT_VIDEO_INDEX = 0;
+  const defaultVideo = videos[DEFAULT_VIDEO_INDEX];
+  videosButtons[DEFAULT_VIDEO_INDEX].classList.add(ACTIVE_BUTTON_CLASS);
+  videoBackground.style.backgroundImage = `url(${PREVIEW_URL}/${parseVideoId(defaultVideo)}/${BACKGROUND_IMAGE})`;
+  playOverlay.addEventListener('click', () => {
+    runVideo(parseVideoId(defaultVideo));
+    hideOverlay();
+  });
+
+  const mySwiper = new Swiper('.swiper-container', {
+    preloadImages: false,
+    lazy: true,
+    slidesPerView: 3,
+    spaceBetween: 5,
+    freeMode: false,
+    slidesPerGroup: 2,
+    breakpoints: {
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+        freeMode: true,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 15,
+        slidesPerGroup: 2,
+        freeMode: true,
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  jarallax(document.querySelectorAll('.jarallax'), {speed: 0.5});
+})();
+
